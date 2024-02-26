2b7c0e25633013e121f2f06d11165cd094e1819e
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-02-26 09:50:36 +0200

    skolas darbs

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
+
 echo "Hello Cruel World";
+
+$connection_string = "mysql:host=localhost;dbname=blog_klavins;user=root;password=;charset=utf8mb4";
+$pdo = new PDO($connection_string);
+
+//Sagatabot SQL izpildei
+$query = $pdo->prepare("SELECT * FROM posts");
+//Izpildit SQL
+$query->execute();
+//Dabut rezultatus
+$posts = $query->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<pre>";
+var_dump($posts);
+echo "</pre>";
