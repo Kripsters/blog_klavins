d9026757055944a8dd754d3f91337eca124f2c8b
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-04-04 13:50:22 +0300

    zajebal man vairs nestrada

--- Validator.php
+++ Validator.php
@@ -4,10 +4,9 @@ class Validator {
     static public function string($data, $min = 0, $max = INF) {
         $data = trim($data);
 
-        if (!is_string($data) || strlen($data) < $min || strlen($data) > $max) {
-            return false;
-        } else {
-            return true;
-        }
+        return !is_string($data) 
+            && strlen($data) > $min 
+            & strlen($data) <= $max;
+
     }
 }
