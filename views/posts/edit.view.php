2581c593ed5f9e91e6a444adfa085c72a6ea3383
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-04-08 10:40:42 +0300

    finished edit validation functionality

--- views/posts/edit.view.php
+++ views/posts/edit.view.php
@@ -1,22 +1,23 @@
 <?php 
 require "views/components/head.php";
 require "views/components/navbar.php";
+
 ?>
 
 <h1>Edit a post</h1>
 <form method="POST">
 <input name="id" value="<?= $post["id"] ?>" type="hidden">
         <label>Title
-            <input name="title" value="<?= $post["title"] ?? "" ?>"/>
+            <input name="title" value="<?= $_POST["title"] ?? $post["title"] ?>"/>
             <?php if (isset($errors["title"])) { ?>
                 <p class="invalid-data"> <?= $errors["title"] ?> </p>
             <?php } ?>
         </label>
         <label>Category_ID
             <select name="cat_id">
-                <option value="1" <?= $post["category_id"] == 1 ? "selected" : "" ?>>sport</option>
-                <option value="2" <?= $post["category_id"] == 2 ? "selected" : "" ?>>music</option>
-                <option value="3" <?= $post["category_id"] == 3 ? "selected" : "" ?>>food</option>
+                <option value="1" <?= ($_POST["cat_id"] ?? $post["category_id"]) == 1 ? "selected" : "" ?> >sport </option>
+                <option value="2" <?= ($_POST["cat_id"] ?? $post["category_id"]) == 2 ? "selected" : "" ?> >music </option>
+                <option value="3" <?= ($_POST["cat_id"] ?? $post["category_id"]) == 3 ? "selected" : "" ?> >food </option>
             </select> 
     <?php if (isset($errors["cat_id"])) { ?>
         <p class="invalid-data"> <?= $errors["cat_id"] ?> </p>
